7e2ed8135cb62ff492320712899f8ee868e6e7a9
DamianoSchirinzi-Dev <DamianoSchirinzi.Dev@gmail.com>
2023-02-27 23:10:03 +0000

    initial commit

---
+++ Animals_API/Model/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Animals_API.Model;
+
+[Table("animals")]
+public partial class Animal
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Name { get; set; }
+
+    [Column("classification")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Classification { get; set; }
+
+    [Column("locations")]
+    [StringLength(260)]
+    [Unicode(false)]
+    public string? Locations { get; set; }
+
+    [Column("is_endangered")]
+    public bool? IsEndangered { get; set; }
+}
---
+++ Animals_API/Model/animals_DB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Animals_API.Model;
+
+public partial class animals_DB : DbContext
+{
+    public animals_DB()
+    {
+    }
+
+    public animals_DB(DbContextOptions<animals_DB> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Animal> Animals { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:damianodevserver.database.windows.net,1433;Initial Catalog=Learning_DB;Persist Security Info=False;User ID=damidev;Password=Bd2d04c5d3d235bc8891da32def1eea315a7fe34\n;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;\n");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Animal>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__animals__3213E83FD6A6B004");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
